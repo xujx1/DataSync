d161bdb048ea2709d894d3b0ab0a52b837baa968
xujinxin <153268908@qq.com>
2016-10-14 10:58:59 +0800

    修复activemq产生的单元测试bug

--- src/test/java/SpringTestCase.java
+++ src/test/java/SpringTestCase.java
@@ -5,7 +5,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 //指定bean注入的配置文件
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:application.xml"})
+@ContextConfiguration(locations = {"classpath:application-test.xml"})
 public class SpringTestCase extends AbstractJUnit4SpringContextTests {
 
 }
